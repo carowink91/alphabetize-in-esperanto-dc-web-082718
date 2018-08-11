d3b0a6324023b2b0ccf0f0882cbea7934b92b645
angelica winkler <caw400@nyu.edu>
2018-08-11 22:27:04 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,6 +4,5 @@ def alphabetize(arr)
   esp_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
   arr = arr.sort_by {|phrase| phrase.split("").collect{|letter| esp_alph.index(letter)}}
-  binding.pry
   arr
 end
